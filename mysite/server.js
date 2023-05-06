ed2d853abade179231bf898dbf33a49b46fadf97
ni3-gaddamwar <131379838+ni3-gaddamwar@users.noreply.github.com>
2023-05-07 00:00:49 +0530

    Update server.js

--- mysite/server.js
+++ mysite/server.js
@@ -1,10 +1,24 @@
-const express = require("express");
-const app = express();
+var express = require('express');
+var path=require('path');
+var app = express();
 
-app.get("/", (request, response) => {
-    response.send("Hi there");
+//server configuration
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(___dirname + '/index.html'));
+ });
+
+
+app.get('/hello', function (req, res) {
+    var person={firstName:'Ravi',lastName:'Tambade',age:47};
+    res.send(person);
 });
 
-app.listen(8888, () => {
-    console.log("hello my site is running")
+var server = app.listen(7667, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://localhost:7667", host, port)
 });
+
+  
