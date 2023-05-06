72e3fd6f53ec26119c2f1ce8e7af691ac0a598fd
ni3-gaddamwar <131379838+ni3-gaddamwar@users.noreply.github.com>
2023-05-06 18:41:35 +0530

    test upload

---
+++ mysite/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send("Hi there");
+});
+
+app.listen(8888, () => {
+    res.sendfile(C:\Users\IET\Desktop\mysite\public\+"about.html");
+});
